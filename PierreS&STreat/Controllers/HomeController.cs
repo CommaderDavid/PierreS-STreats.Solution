518f3b5794a6ac2504a822b4c1debd5dffb36e4c
David Boedigheimer <stealth259@gmail.com>
2021-04-26 16:16:34 -0700

    Initial creation of Peirres.

---
+++ PierreS&STreat/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PierreS_STreat.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PierreS_STreat.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
