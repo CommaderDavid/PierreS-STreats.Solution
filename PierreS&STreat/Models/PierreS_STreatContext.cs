e89bfd65251df423f39be0c7ec5d3942b128196e
David Boedigheimer <stealth259@gmail.com>
2021-05-04 14:54:25 -0700

    Add edits to context and create views.

--- PierreS&STreat/Models/PierreS_STreatContext.cs
+++ PierreS&STreat/Models/PierreS_STreatContext.cs
@@ -6,9 +6,9 @@ namespace PierreS_STreat.Models
 {
     public class PierreS_STreatContext : IdentityDbContext<ApplicationUser>
     {
-        public virtual DbSet<Flavor> Tags { get; set; }
-        public virtual DbSet<Treat> Recipes { get; set; }
-        public virtual DbSet<FlavorTreat> RecipeTag { get; set; }
+        public virtual DbSet<Flavor> Flavors { get; set; }
+        public virtual DbSet<Treat> Treats { get; set; }
+        public virtual DbSet<FlavorTreat> FlavorTreat { get; set; }
 
         public PierreS_STreatContext(DbContextOptions options) : base(options) { }
     }
